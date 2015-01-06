982b7b7c91ad34b2c3311750a44366e8d7f2b366
Timothy Costigan <timvelopment@gmail.com>
2015-01-06 09:48:01 +0000

    expanded fetch. decode, execute loop to allow for more flexible parameters

--- GBEmulator/GbCpu.py
+++ GBEmulator/GbCpu.py
@@ -4,27 +4,32 @@ import GbRegisterSet
 import time
 
 class GbCpu:
+    _i = []
+    _cb = []
+    _iParam = []
+    _cbParam = []
+    for x in range(0x100):
+        _i.append(None)
+        _cb.append(None)
+        _iParam.append(None)
+        _cbParam.append(None)
+
     def __init__(self,memory):
         self._r = GbRegisterSet()
         self._m = memory
-        self._i = []
-        self._cb = []
-        for x in range(0x100):
-            self._i.append(None)
-            self._cb.append(None)
 
     def run(self):
         while True:
             sTime = int(time.time())
             pc = self._r["PC"]
             opcode = self._fetch(pc)
-            instruction = self._decode(opcode,pc)
-            self._execute(instruction,pc)
+            instruction,params = self._decode(opcode,pc)
+            self._execute(instruction,params,pc)
             if instruction != None:
                 self._wait(sTime,instruction.cycles)
 
     def reset(self):
-        pass
+        self._r.reset()
 
     def _fetch(self,pc):
         return self._m[pc]
@@ -32,17 +37,19 @@ class GbCpu:
     def _decode(self,opcode,pc):
         if opcode == 0xCB:
             self._r[pc] = pc+1
-            newOpcode = self._m[pc+1]
-            return self._cb[newOpcode]
+            newOpcode = self._m[self._r[pc]]
+            return self._cb[newOpcode],self._cbParam[newOpcode]
         else:
-            return self._i[opcode]
+            return self._i[opcode],self._iParam[opcode]
 
-    def _execute(self,instruction,pc):
+    def _execute(self,instruction,params,pc):
         if instruction != None:
-            instruction(self,pc)
+            instruction(self,params,pc)
 
     def _wait(self,sTime,cycles):
         pass
+
+    #instructions
 
 
 if __name__ == "__main__":
